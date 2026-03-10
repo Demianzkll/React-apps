453398ede14793a5b2fca9202499869921c06e03
Demian <demanzakaluzhnij@gmail.com>
2026-03-10 12:20:58 +0200

    second commit

--- smart-users-dashboard/src/components/GlobalLoader.jsx
+++
@@ -1,9 +0,0 @@
-import { useIsFetching } from "@tanstack/react-query";
-
-export const GlobalLoader = () => {
-  const isFetching = useIsFetching();
-
-  if (!isFetching) return null;
-
-  return <div className="global-loader">Loading...</div>;
-};
--- smart-users-dashboard/src/store/searchStore.jsx
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const useSearchStore = create((set) => ({
-  search: "",
-  setSearch: (value) => set({ search: value }),
-}));
